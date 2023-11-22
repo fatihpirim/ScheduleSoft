868cb618d89ddf3808dbffdeb2c49f0372d980b9
m_fatih_pirim <muzafferfatihpirim@gmail.com>
2023-11-22 09:49:18 -0700

    moved fxml files to view dir and created customer form & controller

--- src/main/java/com/example/schedulesoft/Main.java
+++ src/main/java/com/example/schedulesoft/Main.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view/Home.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
 
         stage.setMinHeight(700);
