4faf666849f6dc4885dfe7f897ce5f40cd8154ea
m_fatih_pirim <muzafferfatihpirim@gmail.com>
2023-11-22 08:38:18 -0700

    created home.fxml, made some controllers

--- src/main/java/com/example/schedulesoft/Main.java
+++ src/main/java/com/example/schedulesoft/Main.java
@@ -13,13 +13,17 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Main View!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setMinHeight(700);
+        stage.setHeight(700);
+
+        stage.setMinWidth(1000);
+        stage.setWidth(1000);
+
         stage.setScene(scene);
         stage.show();
-        System.out.println("test");
-        System.out.println("changed made in dev");
     }
 
     public static void main(String[] args) {
