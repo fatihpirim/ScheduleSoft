608e390fdd204cd0de1f1085995e79c38e7d078a
m_fatih_pirim <muzafferfatihpirim@gmail.com>
2023-11-23 10:54:21 -0700

    created appointment table skeleton and basic controller functions

--- src/main/java/com/example/schedulesoft/controller/AppointmentTableController.java
+++ src/main/java/com/example/schedulesoft/controller/AppointmentTableController.java
@@ -1,4 +1,26 @@
 package com.example.schedulesoft.controller;
 
+import javafx.fxml.FXML;
+
 public class AppointmentTableController {
+
+    @FXML
+    private void onAdd() {
+
+    }
+
+    @FXML
+    private void onEdit() {
+
+    }
+
+    @FXML
+    private void onDelete() {
+
+    }
+
+    @FXML
+    private void onAdjust() {
+
+    }
 }
