35ede764fe52cf494e0ae328d10afb9d351c6584
m_fatih_pirim <muzafferfatihpirim@gmail.com>
2023-11-22 18:01:57 -0700

    created customer table skeleton and add some on-events to controller

--- src/main/java/com/example/schedulesoft/controller/CustomerTableController.java
+++ src/main/java/com/example/schedulesoft/controller/CustomerTableController.java
@@ -1,4 +1,21 @@
 package com.example.schedulesoft.controller;
 
+import javafx.fxml.FXML;
+
 public class CustomerTableController {
+
+    @FXML
+    private void onAdd() {
+
+    }
+
+    @FXML
+    private void onEdit() {
+
+    }
+
+    @FXML
+    private void onDelete() {
+
+    }
 }
