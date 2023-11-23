d44270d27e9dba087d2ba414c0b46ae788cab8b1
m_fatih_pirim <muzafferfatihpirim@gmail.com>
2023-11-22 17:20:02 -0700

    finished customer add/edit skeleton

--- src/main/java/com/example/schedulesoft/controller/CustomerFormController.java
+++ src/main/java/com/example/schedulesoft/controller/CustomerFormController.java
@@ -1,4 +1,17 @@
 package com.example.schedulesoft.controller;
 
+import javafx.event.Event;
+import javafx.fxml.FXML;
+
 public class CustomerFormController {
+
+    @FXML
+    private void onSave(Event event) {
+        System.out.println("Clicked Save");
+    }
+
+    @FXML
+    private void onCancel(Event event) {
+        System.out.println("Clicked Cancel");
+    }
 }
